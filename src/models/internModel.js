f470774a2bc80dd3b3f44a7362d805a29b44ed2e
avi912patra <avipatra912@gmail.com>
2022-11-21 13:18:59 +0530

    basic code added

---
+++ src/models/internModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+const objectId = mongoose.Schema.Types.ObjectId
+
+const internSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        mobile: {
+            type: String,
+            required: true,
+        },
+        collegeId: {
+            type: objectId,
+            required:true,
+            ref: "CollegeModel"
+
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false
+        }
+    },
+    { timestamps: true });
+
+
+module.exports = mongoose.model('Intern', internSchema)
---
+++ src/routes/route.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.Router();
+
+router.get("/project", function (req, res) {
+    res.send("project is started")
+})
+
+
+module.exports=router
